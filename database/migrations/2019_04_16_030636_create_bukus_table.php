5347a48b33b504954ff1e738507c88c7bd6b6369
rizalgradianto <rigratujuhsatu@gmail.com>
2019-04-25 20:28:44 +0700

    input_data_laravel

---
+++ database/migrations/2019_04_16_030636_create_bukus_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBukusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bukus', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('judul',30);
+            $table->string('penerbit',20);
+            $table->integer('tahun_terbit');
+            $table->string('pengarang',50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bukus');
+    }
+}
